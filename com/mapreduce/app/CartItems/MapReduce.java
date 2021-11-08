93fb48dbfd20d460d33582dab3742d74655d5888
mzoxolombini <88636332+mzoxolombini@users.noreply.github.com>
2021-11-09 00:22:12 +0200

    Added the mapReduce algorithms

---
+++ com/mapreduce/app/CartItems/MapReduce.java
@@ -0,0 +1,75 @@
+package com.mapreduce.app.CartItems;
+
+import java.lang.String;
+import java.io.IOException;
+import java.io.DataInput;
+import java.io.DataOutput;
+import java.util.StringTokenizer;
+import java.util.List;
+import java.util.ArrayList;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+import org.apache.hadoop.io.WritableUtils;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.input.MultipleInputs;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import com.mapreduce.*;
+import com.mapreduce.app.CartItems.CompositeWritable;
+
+public class MapReduce {
+		
+	
+		public static class CartMapper extends Mapper<Object, Text, Text, CompositeWritable>
+		{
+			private Text sales = new Text();
+			
+			public void map(Object key, Text value, Context context) throws IOException, InterruptedException
+			{
+				String[] split = value.toString().split(",");
+				String Event_Type = split[1];
+				String Brand = split[5];
+				if (Event_Type.equals("purchase")) {
+				try
+				{
+					float Price = Float.parseFloat(split[6]);
+					sales.set(Brand);
+					context.write(sales, new CompositeWritable(Price));
+				}
+				catch(NumberFormatException e){}
+				
+				}
+			}
+		}
+	
+		public static class CartReducer extends Reducer<Text,CompositeWritable,Text,FloatWritable>
+		{
+			private FloatWritable result = new FloatWritable();
+			public void reduce(Text key, Iterable<CompositeWritable> values, Context context) throws IOException, InterruptedException
+			{
+				System.out.println("--------------- MIT805 - EXECUTING REDUCER: " + key.toString());
+				
+				
+				int numberOfBrands = 0;
+				float total = 0;
+				for(CompositeWritable val : values)
+				{
+					numberOfBrands = numberOfBrands +1;
+					total += val.price;
+				}																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																												
+				result.set(total / numberOfBrands);
+				context.write(key, result);
+			}
+		}
+
+
+}
---
+++ com/mapreduce/app/CartItems/main.java
@@ -0,0 +1,45 @@
+package com.mapreduce.app.CartItems;
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+import com.mapreduce.app.CartItems.*;
+import com.mapreduce.app.CartItems.MapReduce.CartMapper;
+import com.mapreduce.app.CartItems.MapReduce.CartReducer;
+
+
+
+
+/**
+ * Hello world!
+ *
+ */
+public class main 
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		Configuration conf = new Configuration();
+		//create jpb
+		Job job = Job.getInstance(conf, "Items_on_Carts_Job");
+		job.setJarByClass(MapReduce.class);
+		job.setMapperClass(CartMapper.class);
+		job.setReducerClass(CartReducer.class);
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(CompositeWritable.class);
+		FileInputFormat.addInputPath(job, new Path(args[0]));
+		job.setInputFormatClass(TextInputFormat.class);
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		job.setOutputFormatClass(TextOutputFormat.class);
+		System.exit(job.waitForCompletion(true) ? 0 : 1);
+	}
+}
